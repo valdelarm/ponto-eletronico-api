93b178c6e9eb676ff6e96e082890b6284c7905a5
valdelarm <valdelar.martins@gmail.com>
2017-10-05 13:47:59 -0300

    mapping the entity Empresa

--- src/main/java/com/pontoeletronico/api/entities/Empresa.java
+++ src/main/java/com/pontoeletronico/api/entities/Empresa.java
@@ -1,11 +1,86 @@
 package com.pontoeletronico.api.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Table(name = "EMPRESA")
 public class Empresa implements Serializable {
     private static final long serialVersionUID = 3960436649365666213L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "RAZAO_SOCIAL", nullable = false)
+    private String razaoSocial;
+
+    @Column(name = "CNPJ", nullable = false)
+    private String cnpj;
+
+    @Column(name = "DATA_CRIACAO", nullable = false)
+    private Date dataCriacao;
+
+    @Column(name = "DATA_ATUALIZACAO", nullable = false)
+    private Date dataAtualizacao;
+
+    Empresa(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        dataAtualizacao = new Date();
+    }
+
+    public void prePersist() {
+        final Date atual = new Date();
+        dataCriacao = atual;
+        dataAtualizacao = atual;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa [id=" + id + ", razaoSocial=" + razaoSocial
+                + ", cnpj=" + cnpj + ", dataCriacao=" + dataCriacao + ", dataAtualizacao=" + dataAtualizacao + "]";
+    }
 }
