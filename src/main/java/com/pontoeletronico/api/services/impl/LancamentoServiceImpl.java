96c5a1ef3e6b49d1d0a17edf456de19f984342d8
valdelarm <valdelar.martins@gmail.com>
2017-10-17 23:43:51 -0200

    add lancamento service and its tests

---
+++ src/main/java/com/pontoeletronico/api/services/impl/LancamentoServiceImpl.java
@@ -0,0 +1,49 @@
+package com.pontoeletronico.api.services.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.pontoeletronico.api.entities.Lancamento;
+import com.pontoeletronico.api.repositories.LancamentoRepository;
+import com.pontoeletronico.api.services.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+	
+	private static final Logger log = LoggerFactory.getLogger(LancamentoServiceImpl.class);
+	
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	@Override
+	public Page<Lancamento> buscarPorFuncionarioId(Long funcionarioId, PageRequest pageRequest) {
+		log.info("Buscando lancamentos para o funcionario ID {}", funcionarioId);
+		return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
+	}
+
+	@Override
+	public Optional<Lancamento> buscarPorId(Long id) {
+		log.info("Buscando um lancamento com ID {}", id);
+		return Optional.ofNullable(this.lancamentoRepository.findOne(id));
+	}
+
+	@Override
+	public Lancamento persistir(Lancamento lancamento) {
+		log.info("Salvando o lancamento {}", lancamento);
+		return this.lancamentoRepository.save(lancamento);
+	}
+
+	@Override
+	public void remover(Long id) {
+		log.info("Removendo o lancamento de ID {}", id);
+		this.lancamentoRepository.delete(id);
+		
+	}
+
+}
